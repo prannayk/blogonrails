05369c623e2bd821a840adf3a245d25fb6e1b2f9
Prannay Khosla <prannay.khosla@gmail.com>
2016-02-11 05:31:09 +0800

    Day 1 conclusion, pushing to cloud

--- learning/tasks/app/controllers/tasks_controller.rb
+++ learning/tasks/app/controllers/tasks_controller.rb
@@ -1,4 +1,17 @@
 class TasksController < ApplicationController
   def index
+  	@tasks = Task.all
+  end
+  def create
+  	# attr_accessible :task
+  	# render :text => params.inspect
+  	# Task.create params[:task]
+  	@ad = Task.create(ad_params)
+  	@ad.save
+  	redirect_to :back
+  end
+  private 
+  def ad_params
+  	params.require(:task).permit(:task)
   end
 end
--- learning/tasks/spec/requests/tasks_spec.rb
+++ learning/tasks/spec/requests/tasks_spec.rb
@@ -3,8 +3,19 @@ require 'rails_helper'
 RSpec.describe "Tasks", type: :request do
   describe "GET /tasks" do
     it "dislskjgkdl" do
+    	@task =  Task.create :task => 'go to bed'
     	visit tasks_path
     	page.should have_content 'go to bed'
+    end
+    it "creates a new task" do
+    	visit tasks_path
+    	fill_in 'Task', :with => 'go to work'
+    	click_button 'Create Task'
+
+    	current_path.should == tasks_path
+    	page.should have_content 'go to work'
+    	
+    	save_and_open_page
     end
   end
 end
